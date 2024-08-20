b9bf590a1d85df2746051c8dee65e3bcf3086358
ryananger <anger.ryan.s@gmail.com>
2024-08-20 05:29:48 -0400

    loginForm

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,6 +6,7 @@ import st from 'ryscott-st';
 
 import Landing from './Landing.jsx';
 import Forums from './Forums.jsx';
+import Login from './Login.jsx';
 import ViewBoard from './ViewBoard.jsx';
 
 const isMobile = st.isMobile = window.innerWidth < 1024;
@@ -14,6 +15,7 @@ const route = window.location.pathname.slice(1);
 
 const App = function() {
   const [user, setUser] = st.newState('user', useState(null));
+  const [login, setLogin] = st.newState('login', useState(false));
   const [view, setView] = st.newState('view', useState(!route ? 'landing' : route));
   const [board, setBoard] = st.newState('board', useState(null));
   const [post, setPost] = st.newState('post', useState(null));
@@ -48,9 +50,10 @@ const App = function() {
     <div id='app' className='app texture v'>
       <div className='header h'>
         <a className='forumsTitle' href='/forums'>techTree Academy Forums</a>
-        <IoMenu size={32}/>
+        <div id='loginButton' onClick={()=>{setLogin(true)}}>{!user ? 'login' : 'logout'}</div>
       </div>
       <div className='forums v'>
+        {login && <Login/>}
         {views[view]}
       </div>
     </div>
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react';
+
+import st            from 'ryscott-st';
+import {ax, auth, helpers} from 'util';
+
+const Login = function() {
+  const [signUp, setSignUp] = useState(false);
+
+  var handleSubmit = function(e) {
+    e.preventDefault();
+
+    var form = e.target;
+
+    if (signUp) {
+      if (form.pass.value !== form.pass2.value) {
+        helpers.alert('Passwords do not match!');
+        return;
+      }
+
+      var user = {
+        username: form.username.value,
+        email: form.email.value,
+        password: form.pass.value
+      };
+
+      auth.signUp(user);
+    } else {
+      auth.signIn(form.email.value, form.pass.value);
+    }
+  };
+
+  var renderForm = function() {
+    return (
+      <form id='loginForm' className='loginForm v' onSubmit={handleSubmit} autoComplete='off'>
+        <div className='formHead v'>
+          <h3>
+            Welcome to the techTree Academy Forums!
+          </h3>
+        </div>
+
+        <div className='formBody v'>
+          <div className='loginInputs v'>
+            {signUp && <input className='formInput' name='username' autoComplete='off' type='text' placeholder='username?'/>}
+
+            <input className='formInput' name='email' autoComplete='off' type='text'     placeholder='email address?'/>
+            <input className='formInput' name='pass'  autoComplete='off' type='password' placeholder='password?'/>
+            {signUp && <input className='formInput' name='pass2'  autoComplete='off' type='password' placeholder='confirm it!'/>}
+            <input className='formSubmit' type='submit' value={!signUp ? 'sign in' : 'sign up'}/>
+          </div>
+
+          <div className='signUpText' onClick={()=>{setSignUp(!signUp)}}>
+            {!signUp && 'create an account?'}
+            {signUp  && 'sign in?'}
+          </div>
+        </div>
+
+        <div className='backButton' onClick={()=>{st.setLogin(false)}}>
+          back
+        </div>
+      </form>
+    )
+  };
+
+  return (
+    <div className='auth v'>
+      {renderForm()}
+    </div>
+  )
+};
+
+export default Login;
+
